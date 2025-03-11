2dc39d5a33a2714c8f3113f38b9f5fb87f9a2842
Challelign Tilahun <chalie2123@gmail.com>
2025-03-11 10:17:25 +0300

    get all and single subscription

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,5 @@
 const Subscription = require("../models/subscription.model");
+const User = require("../models/user.model");
 const { log_error } = require("../utils/logger");
 
 const createSubscription = async (req, res, next) => {
@@ -34,5 +35,46 @@ const getUserSubscriptions = async (req, res, next) => {
     next(error);
   }
 };
+const getAllSubscriptions = async (req, res, next) => {
+  try {
+    console.log("All Req new =>", req.headers);
+    const allSubs = await Subscription.find();
 
-module.exports = { createSubscription, getUserSubscriptions };
+    res.status(200).json({ success: true, data: allSubs });
+  } catch (error) {
+    log_error(
+      "[GET_ALL_SUBSCRIPTIONS] " + error.stack.split("\n").join("\n\t")
+    );
+    next(error);
+  }
+};
+
+const getSingleSubscription = async (req, res, next) => {
+  try {
+    console.log("All Req =>", req.headers);
+    console.log("All url =>", req.url, { id: req.params.id });
+    if (!req.params.id) {
+      const error = new Error("Subscription id is not provided");
+      error.status = 401;
+      throw error;
+    }
+    const singleSubs = await Subscription.findById(req.params.id);
+    if (!singleSubs) {
+      const error = new Error("Subscription is not found");
+      error.status = 401;
+      throw error;
+    }
+    res.status(200).json({ success: true, data: singleSubs });
+  } catch (error) {
+    log_error(
+      "[GET_SINGLE_SUBSCRIPTION] " + error.stack.split("\n").join("\n\t")
+    );
+    next(error);
+  }
+};
+module.exports = {
+  createSubscription,
+  getUserSubscriptions,
+  getAllSubscriptions,
+  getSingleSubscription,
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,32 +2,16 @@ const { Router } = require("express");
 const {
   createSubscription,
   getUserSubscriptions,
+  getSingleSubscription,
+  getAllSubscriptions,
 } = require("../controllers/subscription.controller");
 const { authorize } = require("../middlewares/auth.middleware");
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", authorize, createSubscription);
+subscriptionRouter.get("/all-subscription", authorize, getAllSubscriptions);
 subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
-
-subscriptionRouter.get("/:id", (req, res) =>
-  res.send({ title: "GET subscription details" })
-);
-
-subscriptionRouter.put("/:id", (req, res) =>
-  res.send({ title: "UPDATE subscription" })
-);
-
-subscriptionRouter.delete("/:id", (req, res) =>
-  res.send({ title: "DELETE subscription" })
-);
-
-subscriptionRouter.put("/:id/cancel", (req, res) =>
-  res.send({ title: "CANCEL subscription" })
-);
-
-subscriptionRouter.get("/upcoming-renewals", (req, res) =>
-  res.send({ title: "GET upcoming renewals" })
-);
+subscriptionRouter.get("/:id", authorize, getSingleSubscription);
 
 module.exports = subscriptionRouter;
